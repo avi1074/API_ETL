36e100de1a80186e2160826e42518371a44537a9
avi1074 <avi1074@gmail.com>
2023-10-15 11:33:45 -0500

    Fix Jobs, Added Upload File endpoint

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, HTTPException
 from jobs import Jobs
+from io import StringIO
+import pandas as pd
 
 app = FastAPI()
 
@@ -17,7 +19,7 @@ def get_jobs():
     return jobs
 
 @app.post("/jobs/")
-def create_job(job: Job):
+def create_job(job: Jobs):
     jobs.append(job.dict())
     return job
 
@@ -28,3 +30,21 @@ def get_job(job_id: int):
         return job
     else:
         return {"message": "Job not found"}
+
+@app.post("/uploadcsv/")
+async def upload_csv(file: UploadFile = None):
+    if file and file.filename.endswith('.csv'):
+        # Read the file content
+        content = await file.read()
+        
+        # Convert the content to a pandas dataframe
+        try:
+            df = pd.read_csv(StringIO(content.decode('utf-8')))
+            # Here you can do any operation with the dataframe, e.g., save to a database
+            return {"message": f"File '{file.filename}' uploaded successfully!", "data": df.to_dict()}
+        
+        except Exception as e:
+            raise HTTPException(status_code=400, detail="Failed to parse CSV file.")
+    
+    else:
+        raise HTTPException(status_code=400, detail="Invalid file or file type. Only .csv files are supported.")
