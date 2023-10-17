4e4445bbfddfa85dc32b265409ffb0aac72f281f
avi1074 <avi1074@gmail.com>
2023-10-17 09:01:12 -0500

    Added query for mean hired

--- sql_report.py
+++ sql_report.py
@@ -33,3 +33,40 @@ ORDER BY
     job_name;
 """
 
+mean_hired = """
+WITH DepartmentHires2021 AS (
+    SELECT 
+        departments.id AS department_id,
+        departments.departments AS department_name,
+        COUNT(DISTINCT employees.id) AS num_hired
+    FROM 
+        employees 
+    JOIN 
+        departments ON employees.department_id = departments.id
+    WHERE 
+        EXTRACT(YEAR FROM employees.datetime::date) = 2021
+    GROUP BY 
+        departments.id, 
+        departments.departments
+),
+
+AvgHires AS (
+    SELECT 
+        AVG(num_hired) AS avg_hired
+    FROM 
+        DepartmentHires2021
+)
+
+SELECT 
+    department.department_id,
+    department.department_name,
+    department.num_hired
+FROM 
+    DepartmentHires2021 department, AvgHires mean
+WHERE 
+    department.num_hired > mean.avg_hired
+ORDER BY 
+    department.num_hired DESC;
+
+"""
+
